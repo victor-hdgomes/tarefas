423af0106822d81d66a64ef0df370b47ae875ebe
Victor Hugo <88356985+victor-hdgomes@users.noreply.github.com>
2023-09-24 11:26:27 -0300

    Creating signIn and signOut by google
    Creating Header link conditional too

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import { SessionProvider } from 'next-auth/react'
 import Header from '@/components/header'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   )
 }
