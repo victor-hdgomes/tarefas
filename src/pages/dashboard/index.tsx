620636a14e1ab0a05a91db40baf3c15e3a7bb807
Victor Hugo <88356985+victor-hdgomes@users.noreply.github.com>
2023-09-24 12:02:57 -0300

    Creating redirect if doesn't have a user

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,10 @@
+import { GetServerSideProps } from "next"
 import styles from './styles.module.css'
 import Head from 'next/head'
+import { getSession } from "next-auth/react";
 
 export default function Dashboard() {
-    return(
+    return (
         <div className={styles.container}>
             <Head>
                 <title>Meu painel de tarefas</title>
@@ -12,3 +14,20 @@ export default function Dashboard() {
         </div>
     )
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+    const session = await getSession({ req });
+
+    if (!session?.user) {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        };
+    }
+
+    return {
+        props: {},
+    };
+};
