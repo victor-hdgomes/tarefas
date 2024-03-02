9b96c290a1d5e541e9e946cfb28215207014ebea
Victor Hugo <victor.gomes@redefrota.com.br>
2024-03-02 17:45:30 -0300

    Atualizando dados na home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,15 @@ import Image from 'next/image'
 import styles from '@/styles/home.module.css'
 
 import hero from '@/assets/hero.png'
+import { db } from '@/services/firebaseConnection'
+import { collection, getDocs } from 'firebase/firestore'
 
-export default function Home() {
+interface IHome {
+  tarefas: number;
+  comments: number;
+}
+
+export default function Home({ tarefas, comments }: IHome) {
   return (
     <div className={styles.container}>
       <Head>
@@ -15,17 +22,33 @@ export default function Home() {
         <div className={styles.logoContent}>
           <Image className={styles.hero} alt='Logo Tarefas+' src={hero} priority />
           <h1 className={styles.title}>Sistema feito para você organizar <br /> seus estudos e tarefas</h1>
-          
+
           <div className={styles.infoContent}>
             <section className={styles.box}>
-              <span>+12 posts</span>
+              <span>+{tarefas} posts</span>
             </section>
             <section className={styles.box}>
-              <span>+90 comentários</span>
+              <span>+{comments} comentários</span>
             </section>
           </div>
         </div>
       </main>
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const commentRef = collection(db, 'comments');
+  const tarefasRef = collection(db, 'tarefas');
+
+  const commentsSnapshot = await getDocs(commentRef);
+  const tarefasSnapshot = await getDocs(tarefasRef);
+
+  return {
+    props: {
+      tarefas: tarefasSnapshot.size || 0,
+      comments: commentsSnapshot.size || 0
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
+  }
 }
